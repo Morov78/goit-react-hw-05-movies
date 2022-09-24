ab5dc169141fbe23ab2468926e7c0e8b7efd0693
Morov78 <98202937+Morov78@users.noreply.github.com>
2022-09-24 18:42:21 +0300

    Update MovieItemCast.jsx

--- src/components/MovieItem/MovieItemCast/MovieItemCast.jsx
+++ src/components/MovieItem/MovieItemCast/MovieItemCast.jsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const MovieItemCast = () => {
-  const [movieCast, useMovieCast] = useState(null);
-
-  useEffect(() => {});
   return (
     <div>
       <h2>Cast</h2>
