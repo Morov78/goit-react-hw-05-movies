128e5bfd85349b93d1c5535054fc7fa037b0fc9b
Morov78 <98202937+Morov78@users.noreply.github.com>
2022-09-24 18:47:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,22 @@
-// import { fetchMovies } from 'api';
+import { fetchMovies } from 'api';
 import { Home } from 'page/Home';
 import { Movies } from 'page/Movies';
 import { MovieItem } from 'components/MovieItem/MovieItem';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { MovieItemCast } from './MovieItem/MovieItemCast/MovieItemCast';
-import axios from 'axios';
-
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '092bcb29ccd47efc5792e9a4733d23fe';
 
 export const App = () => {
   const [dataMovies, setDataMovies] = useState(null);
-
+  const fetch = useRef(false);
   useEffect(() => {
-    const fetch = async () => {
-      const responce = await axios(
-        `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
-      );
-      setDataMovies(responce.data.results);
-    };
     console.log('Mouting phase: same when componentDidMount runs');
 
-    fetch();
+    fetchMovies().then(responce => {
+      setDataMovies(responce.data.results);
+      fetch.current = true;
+    });
   }, []);
 
   return (
